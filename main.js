433a4f213d44e0cc7d3413dc4e56aee9dab3ba4e
Breno-Coelho <brenocoelho004@gmail.com>
2021-08-17 15:17:59 -0300

    Fim da aula 2

---
+++ main.js
@@ -0,0 +1,30 @@
+/*Abre e fecha o menu, vulgo toggle*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/*Esconde o menu quando algum item é clicado*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/*mudar o header quanto tiver scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
